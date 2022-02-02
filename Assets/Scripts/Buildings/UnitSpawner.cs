1da05842c17b461a3cd99cbb59e4109844793d2e
Laurence279 <lnunn279@gmail.com>
2022-02-02 08:33:02 +0000

    Add unit spawner to each player and spawn units manually

---
+++ Assets/Scripts/Buildings/UnitSpawner.cs
@@ -0,0 +1,40 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UnitSpawner : NetworkBehaviour, IPointerClickHandler
+
+{
+    [SerializeField] private GameObject unitPrefab = null;
+    [SerializeField] private Transform spawnPoint = null;
+
+
+    #region Server
+
+    [Command]
+    private void CmdSpawnUnit()
+    {
+        GameObject unitInstance = Instantiate(unitPrefab, spawnPoint.position, spawnPoint.rotation);
+
+        NetworkServer.Spawn(unitInstance, connectionToClient);
+
+    }
+
+    #endregion
+
+    #region Client
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if(eventData.button != PointerEventData.InputButton.Left) { return; }
+
+        if(!hasAuthority) { return; }
+
+        CmdSpawnUnit();
+    }
+
+
+    #endregion
+}
